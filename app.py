c1fef58b97f2d780c7bf4c27e02cdbe60d1580cc
marykuo <meiyu19980928@gmail.com>
2025-11-06 22:03:46 +0800

    feat: load and save cache periodically

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify, request
 import datetime
+import threading
+import time
+
+import json
+import os
 
 
 app = Flask(__name__)
 
 # In-memory cache for game rankings
 cache = {}
+MINUTES_IN_SECOND = 60
 
 
 @app.route("/")
@@ -37,5 +43,31 @@ def get_ranks():
     return jsonify([]), 200
 
 
+# Background scheduler to save cache
+def save_cache():
+    while True:
+        print("Saving cache to cache.json")
+        with open("cache.json", "w", encoding="utf-8") as f:
+            json.dump(cache, f, ensure_ascii=False, indent=2)
+        time.sleep(MINUTES_IN_SECOND * 60)
+
+
+# Load cache from file
+def load_cache():
+    if not os.path.exists("cache.json"):
+        return
+
+    try:
+        print("Loading cache from cache.json")
+        with open("cache.json", "r", encoding="utf-8") as f:
+            cache.update(json.load(f))
+        print("Cache loaded successfully")
+    except Exception as e:
+        print(f"Failed to load cache.json: {e}")
+
+
 if __name__ == "__main__":
+    load_cache()
+    thread = threading.Thread(target=save_cache, daemon=True)
+    thread.start()
     app.run(host="0.0.0.0", port=8080)
